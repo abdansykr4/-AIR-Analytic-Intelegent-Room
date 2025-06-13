8342db35caf28b993dd561d98e14fa032e728993
abdansykr4 <156106365+abdansykr4@users.noreply.github.com>
2025-06-13 11:04:58 +0700

    Clean

--- script.js
+++
@@ -1,7 +0,0 @@
-// For future dynamic behavior, like fetching sensor data
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Dashboard loaded successfully!");
-    // Example to show class status update dynamically
-    let class4 = document.getElementById('class4');
-    class4.querySelector('.status').textContent = 'Peringatan: Gas CO2 tinggi!';
-});
